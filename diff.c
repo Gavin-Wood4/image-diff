ba0d26ce664299b52255665569abaaa085e467b1
Gavin Wood <gavson03@gmail.com>
2025-05-20 00:39:24 -0400

    Added diff.c and neon-diff.c image subtraction programs.

---
+++ diff.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/*
+The purpose of this program is to subtract one image layer's RGB values from
+another and output it's result in .rgba format. 
+*/
+
+ 
+int read_rgba(const char *filename, uint32_t **buf, size_t *size);
+void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size);
+int write_rgba(const char *filename, uint32_t *buf, size_t size);
+
+
+int main(int argc, char *argv[])
+{
+	char arg_err_msg[] = "Incorrect number of arguments passed (must be 3.)";
+	if (argc != 4)
+		return printf(arg_err_msg);
+
+	uint32_t *img1 = NULL;
+	uint32_t *img2 = NULL;
+	size_t size1, size2;
+
+	if (read_rgba(argv[1], &img1, &size1) == -1)
+		goto err;
+	if (read_rgba(argv[2], &img2, &size2) == -1)
+		goto err;
+	diff_rgba(img1, img2, size1);
+	if (write_rgba(argv[3], img1, size1) == -1) {
+		goto err;
+	}
+	return 0;
+
+err:	// Dump memory if image error
+	free(img1);
+	free(img2);
+	return printf("There was an error with one of the image files.");
+}
+
+int read_rgba(const char *filename, uint32_t **buf, size_t *size)
+{
+	struct stat st;
+	if (stat(filename, &st) == -1)
+		return -1;
+	*size = st.st_size;
+	*buf = malloc(*size); //Allocate buffer to be large enough for size
+	if (*buf == NULL) {
+		return -1;
+	}
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1) {	// If file descriptor error then free buffer
+		free(*buf);
+		*buf = NULL;
+		return -1;
+	}
+	ssize_t	bytes_read = read(fd, *buf, *size); // Assign bytes_read errchk
+	close(fd);
+	if (bytes_read != *size) { 
+		free(*buf);
+		*buf = NULL;
+		return -1;
+	}
+	return 0;
+}
+
+void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size)
+{
+	size_t num_pixels = size/sizeof(uint32_t); // calculate number of pixels
+	for (size_t i = 0; i < num_pixels; i++) {
+		uint32_t pix1 = img1[i];
+		uint32_t pix2 = img2[i];
+		img1[i] = ((pix1 - pix2) & 0x00FFFFFF) | 0xFF000000; // Pix -
+	}
+}
+
+int write_rgba(const char *filename, uint32_t *buf, size_t size) {
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (fd == -1)
+		return -1;
+	ssize_t bytes_written = write(fd, buf, size);
+	close(fd);
+	if (bytes_written != size)
+		return -1;
+	free(buf);
+	return 0;
+}
