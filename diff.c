8a31b34a93266710f3e6673e26870a35b1fd5117
Gavin Wood <gavson03@gmail.com>
2025-05-22 16:18:55 -0400

    Added subtraction saturation to diff.c to mimick the functionality of neon-diff.c's diff_rgba function.

--- diff.c
+++ diff.c
@@ -5,7 +5,8 @@ The purpose of this program is to subtract one image layer's RGB values from
 another and output it's result in .rgba format. 
 */
 
-void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size);
+static inline uint32_t abs_diff_pixel(uint32_t pix1, uint32_t pix2);
+static void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size);
 
 int main(int argc, char *argv[])
 {
@@ -28,21 +29,31 @@ int main(int argc, char *argv[])
 	}
 	return EXIT_SUCCESS;
 
-err:	// Dump memory if image error
+err:	// Dumps memory if image reading or writing error.
 	if (img1) free(img1);
 	if (img2) free(img2);
 	fprintf(stderr, "There was an error with one of the image files.\n");
 	return EXIT_FAILURE;
 }
 
-
-void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size)
+static inline uint32_t abs_diff_pixel(uint32_t pix1, uint32_t pix2)
 {
-	size_t num_pixels = size/sizeof(uint32_t); // calculate number of pixels
-	for (size_t i = 0; i < num_pixels; i++) {
-		uint32_t pix1 = img1[i];
-		uint32_t pix2 = img2[i];
-		img1[i] = ((pix1 - pix2) & 0x00FFFFFF) | 0xFF000000; // Pix -
+	uint32_t pixout = 0;
+	int channel1, channel2, signed_difference;
+	for (int shift = 0; shift < 32; shift += 8) {
+		channel1 = (pix1 >> shift) & 0xFF;	// Extract the first color channel.
+		channel2 = (pix2 >> shift) & 0xFF;
+		signed_difference = channel1 - channel2;
+		if (signed_difference < 0) signed_difference = -signed_difference;
+		pixout |= (uint32_t)d << shift;		// Place pixel back in place.
 	}
+	return pixout;
+}
+
+static void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size)
+{
+	size_t i = 0;
+	for (i; i < n; i++)
+		img1[i] = abs_diff_pixel(img1[i], img2[i]);
 }
 
