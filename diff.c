09db096071de2efbf110cff2f46ba6b062dbe030
Gavin Wood <gavson03@gmail.com>
2025-05-26 20:22:51 -0400

    Added algorithm and functionality for a fourth argument selectable absolute, modular, or saturated subtraction modes in diff.c

--- diff.c
+++ diff.c
@@ -3,20 +3,37 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 
 /*
 The purpose of this program is to subtract one image layer's RGB values from
 another and output it's result in .rgba format. 
 */
 
-static inline uint32_t abs_diff_pixel(uint32_t pix1, uint32_t pix2);
-static void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size);
+typedef enum { ABS, SAT, MOD } diff_mode_t;
+
+static void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size, diff_mode_t mode);
 
 int main(int argc, char *argv[])
 {
-	if (argc != 4) {
-		fprintf(stderr, "Usage: %s <image1> <image2> <output>\n", argv[0]);
+	if (argc != 4 && argc != 5) {
+		fprintf(stderr, "Usage: %s <image1> <image2> <output> <absolute|saturated|modular>\n", argv[0]);
 		return EXIT_FAILURE;
+	}
+
+	diff_mode_t mode = ABS;
+	if (argc == 5) {
+		if ((strcmp(argv[4], "saturated") == 0)||(strcmp(argv[4], "sat") == 0)) {
+			mode = SAT;
+		} else if ((strcmp(argv[4], "modular") == 0)||(strcmp(argv[4], "mod") == 0)) {
+			mode = MOD;
+		} else if ((strcmp(argv[4], "absolute") == 0)||(strcmp(argv[4], "abs") == 0)) {
+			mode = ABS;
+		}
+		else {
+			fprintf(stderr, "Error: Invalid mode '%s'.\nUse: absolute (abs), saturated (sat), or modular (mod).\n", argv[4]);
+			return EXIT_FAILURE;
+		}
 	}
 
 	uint32_t *img1 = NULL;
@@ -24,11 +41,11 @@ int main(int argc, char *argv[])
 	size_t size1, size2;
 
 	if (read_rgba(argv[1], &img1, &size1) == -1) {
-		fprintf(stderr, "Error: Could not read %s.\n", argv[1]);
+		fprintf(stderr, "Error: Could not read '%s'.\n", argv[1]);
 		goto err;
 	}
 	if (read_rgba(argv[2], &img2, &size2) == -1) {
-		fprintf(stderr, "Error: Could not read %s.\n", argv[2]);
+		fprintf(stderr, "Error: Could not read '%s'.\n", argv[2]);
 		goto err;
 	}
 
@@ -36,7 +53,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Images must be the same dimensions.\n");
 		goto err;
 	}
-	diff_rgba(img1, img2, size1);
+	diff_rgba(img1, img2, size1, mode);
 	
 	if (write_rgba(argv[3], img1, size1) == -1) {
 		fprintf(stderr, "Error: Unable to write the new image.\n");
@@ -51,26 +68,39 @@ err:	// Dumps memory if image reading or writing error.
 	return EXIT_FAILURE;
 }
 
-static inline uint32_t abs_diff_pixel(uint32_t pix1, uint32_t pix2)
+static void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size, diff_mode_t mode)
 {
-	uint32_t pixout = 0;
-	int channel1, channel2, signed_difference;
-	int shift = 0;
-	for (; shift < 32; shift += 8) {
-		channel1 = (pix1 >> shift) & 0xFF;	// Extract the first color channel.
-		channel2 = (pix2 >> shift) & 0xFF;
-		signed_difference = channel1 - channel2;
-		if (signed_difference < 0) signed_difference = -signed_difference;
-		pixout |= (uint32_t)signed_difference << shift;		// Place pixel back in place.
-	}
-	return pixout |= 0xFF000000;	// This forces opacity at 100%.
-}
-
-static void diff_rgba(uint32_t *img1, const uint32_t *img2, size_t size)
-{
-	size_t pxs = size / sizeof(uint32_t);
+	size_t pxs = size / sizeof(uint32_t);	// pxs represents the number of pixels in the image.
 	size_t i = 0;
-	for (; i < pxs; ++i)
-		img1[i] = abs_diff_pixel(img1[i], img2[i]);
+	uint32_t pix1, pix2, pixout;
+	for (; i < pxs; ++i) {
+		pix1 = img1[i];
+		pix2 = img2[i];
+		pixout = 0;
+		int channel1, channel2, difference;
+		uint8_t output_channel;
+		
+		int shift;
+		for (shift = 0; shift <= 16; shift += 8) {
+			channel1 = (pix1 >> shift) & 0xFF;	// This isolates one color channel, R(shift = 0) G(shift = 8), B (shift = 16)
+			channel2 = (pix2 >> shift) & 0xFF;	// The alpha channel is skipped so that images are not transparent (unintended behavior). 
+			difference = channel1 - channel2;
+
+			switch (mode) {
+				case SAT:
+					output_channel = (difference < 0) ? 0 : (uint8_t)difference;	// If the computed difference is smaller than 0, clamp to 0.
+					break;
+				case MOD:
+					output_channel = (uint8_t)difference;				// Implicit declaration to unsigned for modulo 256 subtraction.
+					break;
+				case ABS:
+				default:
+					output_channel = (difference < 0) ? (uint8_t)(-difference) : (uint8_t)difference;	// Change sign of difference to find absolute value if negative.
+					break;
+			}
+			pixout |= (uint32_t)output_channel << shift;
+		}
+		img1[i] = pixout | 0xFF000000;	// Forces 100% opacity.
+	}
 }
 
